2d6c8f12de95a3db2ad3bf8df558b1adf47cf649
tlesner <tjlesner@gmail.com>
2025-04-17 20:01:22 -0600

    render deploy troubleshooting

--- server/src/server.ts
+++ server/src/server.ts
@@ -26,19 +26,21 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
 	await server.start();
 	await db;
-
-	// const PORT = process.env.PORT || 3001;
-	// const app = express();
-
 	app.use(express.urlencoded({ extended: false }));
 	app.use(express.json());
-
-	app.use(
-		'/graphql',
-		expressMiddleware(server as any, {
-			context: authenticateToken as any,
-		})
-	);
+	app.use('/graphql', expressMiddleware(server as any,
+	  {
+		context: authenticateToken as any
+	  }
+	));
+	if (process.env.NODE_ENV === 'production') {
+	  console.log('running in production')
+	  app.use(express.static(path.join(__dirname, '../../client/dist')));
+	  app.get('*', (_req: Request, res: Response) => {
+		res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+	  });
+	}
+  
 
 	if (process.env.NODE_ENV === 'production') {
 		app.use(express.static(path.join(__dirname, '../client/dist')));
